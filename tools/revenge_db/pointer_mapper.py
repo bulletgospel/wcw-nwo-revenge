6302309dfeee1e61113790f9426d2cdd0fe53c90
bulletgospel <XP5AC0@GMAIL.COM>
2026-06-23 00:04:15 -0400

    Add pointer delta analysis

--- tools/revenge_db/pointer_mapper.py
+++ tools/revenge_db/pointer_mapper.py
@@ -1,7 +1,26 @@
 from data.wrestlers import WRESTLERS
 
 
+def parse_pointer(value):
+    if not value:
+        return None
+
+    return int(value, 16)
+
+
+def pointer_delta(start, end):
+    if start is None or end is None:
+        return None
+
+    return end - start
+
+
 def print_pointer_map(wrestler):
+    name_pointer = parse_pointer(wrestler["name_pointer"])
+    height_pointer = parse_pointer(wrestler["height_pointer"])
+    weight_pointer = parse_pointer(wrestler["weight_pointer"])
+    stable_pointer = parse_pointer(wrestler["stable_pointer"])
+
     print(wrestler["name"])
     print(f'ID2: {wrestler["id2"]}')
     print(f'ID4: {wrestler["id4"]}')
@@ -9,6 +28,7 @@ def print_pointer_map(wrestler):
     print(f'Height Pointer: {wrestler["height_pointer"]}')
     print(f'Weight Pointer: {wrestler["weight_pointer"]}')
     print(f'Stable Pointer: {wrestler["stable_pointer"]}')
+    print(f'Stable - Name Delta: 0x{pointer_delta(name_pointer, stable_pointer):X}')
 
 
 def main():
